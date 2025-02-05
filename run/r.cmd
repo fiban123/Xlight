7afda8fa5cdf075d0228266ac8da2904c09302a9
fiban123 <fabianblatter09@gmail.com>
2025-02-05 10:30:59 +0100

    4

--- run/r.cmd
+++ run/r.cmd
@@ -1,3 +1,3 @@
-g++ ../src/xlight.cpp -o xlight.exe -lportaudio -lfftw3f -lraylib -lopengl32 -lgdi32 -lwinmm -pthread -Wall -static-libgcc -static-libstdc++ -std=c++23 -m64 -lole32
+g++ ../src/xlight.cpp -o xlight.exe -lportaudio.dll -lfftw3f -lraylib -lopengl32 -lgdi32 -lwinmm -pthread -Wall -static-libgcc -static-libstdc++ -std=c++23 -m64 -lole32 -static
 
 .\xlight
