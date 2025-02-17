6fbe2ce0786f6144117e2d6546f5f10beb36f525
fiban123 <fabianblatter09@gmail.com>
2025-02-17 16:47:29 +0100

    clear branch

--- run/r.cmd
+++
@@ -1,3 +0,0 @@
-g++ ../src/xlight.cpp -o xlight.exe -lportaudio.dll -lfftw3f -lraylib -lopengl32 -lgdi32 -lwinmm -pthread -Wall -static-libgcc -static-libstdc++ -std=c++23 -m64 -lole32 -static
-
-.\xlight
